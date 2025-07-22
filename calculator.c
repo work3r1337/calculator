d353ae3aad3aca479d4f579eecd3a3cb36cb286b
work3r1337 <c.michasov2004@yandex.ru>
2025-07-22 23:32:44 +0300

    Add unary minus

--- calculator.c
+++ calculator.c
@@ -7,7 +7,9 @@
 #define MAX_TOKEN_SIZE 10
 #define MAX_BUFFER_SIZE 100
 #define VALID_TOKENS \
-    { "+", "-", "*", "/", "(", ")" }
+    { "+", "-", "*", "/", "(", ")", "u" }
+#define VALID_TOKENS_SIZE \
+    (int)(sizeof(valid_tokens) / (sizeof(char) * MAX_TOKEN_SIZE))
 
 typedef enum { TOKEN_NUMBER, TOKEN_OPERATOR, TOKEN_END } TokenType;
 
@@ -22,19 +24,18 @@ typedef struct {
     Node *top;
 } Stack;
 
-void tokenization(char str[]);
+Status tokenization(char str[]);
 Status infix_to_postfix(char str[]);
 Status calc_rpn(char str[], double *result);
 Status validate(char str[]);
-bool valid(char token[]);
 int get_priority(const char token[]);
-TokenType get_token(char **str, char token[]);
+TokenType get_token(char **str, char token[], TokenType last_token);
 char getch(char **str);
 void ungetch(char **str);
 int is_empty(const Stack *stack_p);
 void push(Stack *stack_p, const char token[]);
 Status pop(Stack *stack_p, char token[]);
-Status peek(Stack *stack_p, char token[]);
+Status peek(const Stack *stack_p, char token[]);
 void clean(Stack *stack_p);
 
 int main() {
@@ -42,12 +43,18 @@ int main() {
         char str[MAX_BUFFER_SIZE];
         int index = 0;
         printf("Exit(q)\nExpression: ");
-        while ((str[index++] = getchar()) != '\n');
+        while ((str[index++] = getchar()) != '\n') {
+            if (index > MAX_BUFFER_SIZE - 1) {
+                printf("n/a");
+                while (getchar() != '\n');
+                continue;
+            }
+        }
         if (index == 2 && str[0] == 'q') break;
         str[index] = '\0';
-        tokenization(str);
         double result;
-        if (validate(str) == SUCCESS && infix_to_postfix(str) == SUCCESS &&
+        if (tokenization(str) == SUCCESS && validate(str) == SUCCESS &&
+            infix_to_postfix(str) == SUCCESS &&
             calc_rpn(str, &result) == SUCCESS)
             printf("Result: %lf\n", result);
         else
@@ -57,24 +64,31 @@ int main() {
     return 0;
 }
 
-void tokenization(char str[]) {
+Status tokenization(char str[]) {
     char buffer[MAX_BUFFER_SIZE];
     char token[MAX_TOKEN_SIZE];
     char *tmp = str;
     int index = 0;
+    Status status = SUCCESS;
+    TokenType last_type = TOKEN_OPERATOR, type;
     *strchr(str, '\n') = '\0';
-    while (get_token(&tmp, token) != TOKEN_END) {
+    while (status == SUCCESS &&
+           (type = get_token(&tmp, token, last_type)) != TOKEN_END) {
         int i = 0;
-        while (token[i] != '\0') buffer[index++] = token[i++];
+        while (status == SUCCESS && token[i] != '\0') {
+            buffer[index++] = token[i++];
+            if (index >= MAX_BUFFER_SIZE - 1) status = ERROR;
+        }
         buffer[index++] = ' ';
+        last_type = type;
     }
     buffer[--index] = '\0';
     memcpy(str, buffer, MAX_BUFFER_SIZE);
+    return status;
 }
 
 Status infix_to_postfix(char str[]) {
     TokenType type;
-    TokenType last_type = TOKEN_END;
     char buffer[MAX_BUFFER_SIZE];
     char token[MAX_TOKEN_SIZE], tmp_token[MAX_TOKEN_SIZE];
     char *tmp = str;
@@ -82,7 +96,7 @@ Status infix_to_postfix(char str[]) {
     Stack stack;
     stack.top = NULL;
     Status status = SUCCESS;
-    while ((type = get_token(&tmp, token)) != TOKEN_END) {
+    while ((type = get_token(&tmp, token, TOKEN_END)) != TOKEN_END) {
         if (type == TOKEN_NUMBER) {
             int i = 0;
             while (token[i] != '\0') buffer[index++] = token[i++];
@@ -98,7 +112,7 @@ Status infix_to_postfix(char str[]) {
                 if (pop(&stack, tmp_token) == ERROR) status = ERROR;
             }
         } else {
-            if (!is_empty) peek(&stack, tmp_token);
+            if (!is_empty(&stack)) peek(&stack, tmp_token);
             while (!is_empty(&stack) &&
                    get_priority(tmp_token) >= get_priority(token)) {
                 if (pop(&stack, tmp_token) == ERROR) status = ERROR;
@@ -108,7 +122,6 @@ Status infix_to_postfix(char str[]) {
             }
             push(&stack, token);
         }
-        last_type = type;
     }
     while (!is_empty(&stack)) {
         if (pop(&stack, tmp_token) == ERROR) status = ERROR;
@@ -131,7 +144,7 @@ Status calc_rpn(char str[], double *result) {
     stack.top = NULL;
     Status status = SUCCESS;
 
-    while ((type = get_token(&str, token)) != TOKEN_END) {
+    while ((type = get_token(&str, token, TOKEN_END)) != TOKEN_END) {
         if (type == TOKEN_NUMBER)
             push(&stack, token);
         else if (strcmp(token, "+") == 0) {
@@ -157,10 +170,14 @@ Status calc_rpn(char str[], double *result) {
                 push(&stack, tmp);
             } else
                 status = ERROR;
+        } else if (strcmp(token, "u") == 0) {
+            if (pop(&stack, tmp_token1) == ERROR) status = ERROR;
+            sprintf(tmp, "%lf", -atof(tmp_token1));
+            push(&stack, tmp);
         } else
             status = ERROR;
     }
-    pop(&stack, tmp_token1);
+    if (pop(&stack, tmp_token1) == ERROR) status = ERROR;
     clean(&stack);
     *result = atof(tmp_token1);
     return status;
@@ -172,12 +189,10 @@ Status validate(char str[]) {
     char token[MAX_TOKEN_SIZE];
     char valid_tokens[][MAX_TOKEN_SIZE] = VALID_TOKENS;
     Status status = SUCCESS;
-    while ((type = get_token(&str, token)) != TOKEN_END) {
+    while ((type = get_token(&str, token, TOKEN_END)) != TOKEN_END) {
         if (type == TOKEN_OPERATOR) {
             bool is_valid = false;
-            for (int i = 0;
-                 i < sizeof(valid_tokens) / (sizeof(char) * MAX_TOKEN_SIZE);
-                 i++) {
+            for (int i = 0; i < VALID_TOKENS_SIZE; i++) {
                 if (strcmp(token, valid_tokens[i]) == 0) is_valid = true;
             }
             if (!is_valid) status = ERROR;
@@ -191,24 +206,18 @@ Status validate(char str[]) {
     return status;
 }
 
-bool valid(char token[]) {
-    int tokens_count = 4;
-    char suitable_tokens[][MAX_TOKEN_SIZE] = {"+", "-", "*", "/"};
-    for (int i = 0; i < tokens_count; i++)
-        if (strcmp(token, suitable_tokens[i]) == 0) return true;
-    return false;
-}
-
 int get_priority(const char token[]) {
     if (strcmp(token, "+") == 0 || strcmp(token, "-") == 0)
         return 1;
     else if (strcmp(token, "*") == 0 || strcmp(token, "/") == 0)
         return 2;
+    else if (strcmp(token, "u") == 0)
+        return 3;
     else
         return 0;
 }
 
-TokenType get_token(char **str, char token[]) {
+TokenType get_token(char **str, char token[], TokenType last_type) {
     int i = 0;
     while ((token[i] = getch(str)) == ' ' || token[i] == '\t');
     token[1] = '\0';
@@ -221,6 +230,9 @@ TokenType get_token(char **str, char token[]) {
         token[i] = '\0';
         ungetch(str);
         return TOKEN_NUMBER;
+    } else if (token[0] == '-' && last_type == TOKEN_OPERATOR) {
+        token[0] = 'u';
+        return TOKEN_OPERATOR;
     } else {
         while (isalpha(token[++i] = getch(str)));
         token[i] = '\0';
@@ -251,7 +263,7 @@ Status pop(Stack *stack_p, char token[]) {
     return SUCCESS;
 }
 
-Status peak(Stack *stack_p, char token[]) {
+Status peek(const Stack *stack_p, char token[]) {
     if (is_empty(stack_p)) return ERROR;
     memcpy(token, stack_p->top->token, MAX_TOKEN_SIZE);
     return SUCCESS;
